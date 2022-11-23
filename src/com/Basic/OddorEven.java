701bd26d7a1b70feffd4c21fa8194edb503f0f30
Atulsak <sakhareat@gmail.com>
2022-11-23 15:11:44 +0530

    ADDED:Java Basic Core

---
+++ src/com/Basic/OddorEven.java
@@ -0,0 +1,24 @@
+package com.Basic;
+import java.util.Scanner;
+
+
+public class OddorEven {
+	public static void main(String[] args) {
+		System.out.println("Welcome To Even Or Odd Program");
+		Scanner scanner = new Scanner(System.in);
+		// INPUT number
+		int number = 0;
+		System.out.print("Enter number : ");
+		number = scanner.nextInt();
+		System.out.println("You entered "+number);
+
+		if (number % 2 == 0) {
+			System.out.println(number+" is Even");
+		} else {
+			System.out.println(number+" is Odd");
+		}
+		scanner.close();
+	}
+	}
+
+
